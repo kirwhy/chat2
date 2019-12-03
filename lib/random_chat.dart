c1276a9b52d26d7b62402f3d340cdfe7282850df
Diego Laballós <diego@diegolaballos.com>
2019-12-03 17:09:01 +0100

    Add example project

--- lib/random_chat.dart
+++ lib/random_chat.dart
@@ -1,7 +1,8 @@
 library random_chat;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class RandomChat {
+  
+  String getValue() {
+    return "Hello Package";
+  }
 }
